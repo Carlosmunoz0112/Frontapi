35e148126584c35f1fe23c84c74c1fe7344da4a5
Carlosmunoz0112 <carlosmunoz0112@gmail.com>
2023-09-21 08:34:11 -0500

    V3

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://apifront.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'https://apifinal2109.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
