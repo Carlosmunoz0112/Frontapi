31fa9b7cae0801f8216b1f2d8b431c4ed5b00fdb
Carlosmunoz0112 <carlosmunoz0112@gmail.com>
2023-09-21 08:29:52 -0500

    v2

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ app.use((req, res, next) => {
     next();
   });
 
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://apifront.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 // Puerto
 const port = process.env.PORT || 3000;
 
